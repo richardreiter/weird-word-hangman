d9b338b8a2ef313e6d20fe29ad6d8bd1f7501677
richardreiter <richard.reiter.dev@gmail.com>
2021-11-23 16:25:19 +0000

    Add a test word and print variable to store partially shown word

--- run.py
+++ run.py
@@ -6,3 +6,9 @@ from words import weird_words  # imports weird word list from words.py file
 def pick_word():
     actual_word = random.choice(weird_words)
     return actual_word.upper()
+
+
+# make a list of underscores of current word's length
+current_word = "TEST"
+show_word = list(len(current_word)*"_")
+print(show_word)
