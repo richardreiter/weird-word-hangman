9af4dd9acf2806f58651a0e6d67f225c9218815c
richardreiter <richard.reiter.dev@gmail.com>
2021-11-23 01:06:35 +0000

    Import random library and create a function to get a random word from imported list

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from words import weird_words
+import random  # imports random library to pick a word randomly
+from words import weird_words  # imports weird word list from words.py file
 
-print(weird_words)
+
+# function to randomly choose a word from weird words list
+def pick_word():
+    actual_word = random.choice(weird_words)
+    return actual_word.upper()
